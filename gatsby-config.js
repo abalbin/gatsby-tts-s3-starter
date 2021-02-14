b99fc8d61d2273fc635bfd865e5c23fe1da40cb5
Arturo Balbin <arturo.balbin@keynua.com>
2021-02-14 17:54:12 -0500

    add missing info for packaging

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
 		{
 			resolve: `gatsby-plugin-manifest`,
 			options: {
-				name: `gatsby-tts-s3-default`,
+				name: `gatsby-tts-s3-starter`,
 				short_name: `starter`,
 				start_url: `/`,
 				background_color: `#663399`,
